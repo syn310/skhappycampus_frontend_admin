7867d4bb6f92fa9d11b7a60fd6b6daeca4213e2d
khs <noisonnoiton@gmail.com>
2020-02-28 13:36:02 +0900

    port ip edit;;

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,12 +18,12 @@ module.exports = {
         contentBase: __dirname + '/dist/',
         proxy: {
           '/nice': {
-            target: 'http://192.168.0.10:8081', //nice
+            target: 'http://192.168.0.8:8081', //nice
                 secure: false,
                 changeOrigin: true,
            },
           '/': {
-              target: 'http://localhost:3006', //api
+              target: 'http://192.168.0.8:3008', //api
                   secure: false,
                   changeOrigin: true,
           }
